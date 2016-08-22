967ad38ff28f0bf523bee5d75d0eb6a039534e32
palebeatz <timothyry@msn.com>
2016-08-22 11:51:08 -0600

    Added posts and handles

--- app/helpers/handles.rb
+++ app/helpers/handles.rb
@@ -2,13 +2,20 @@ module Handles
   PolHandles = ["HillaryClinton", "SenSanders", "realDonaldTrump", "JimWebbUSA", "RealBenCarson", "ChrisChristie", "JoeBiden", "BarackObama", "PRyan",
                                 "SarahPalinUSA", "SpeakerBoehner", "newtgingrich", "GovGaryJohnson", "THEHermanCain", "sen.franken", "Schwarzenegger", "nicolassarkozy", 
                                 "georgewbush", "algore", "ronpaul", "DWStweets", "EricCantor", "CarlyFiorina", "JebBush", "marcorubio", "DrJillStein", "JohnKasich",
-                                "GovWalker"]
+                                "GovWalker", "JoeLieberman", "BachusAL06", "PeteSessions", "RepBillShuster", "WhipHoyer",
+                                "DarrellIssa", "cbrangel", "RepMarryFallin", "SenJohnMcCain", "DanaRohrabacher", "DavidVitter", 
+                                "OrrinHatch", "lisamurkowski", "SenatorReid", "russfeingold", "Dennis_Kucinich", "JimDeMint"]
                                 
   CelHandles = ["TheRealJRSmith", "JoelEmbiid", "n3twork", "ZachLowe_NBA", "BillSimmons", "AminESPN", "NateDuncanNBA", "HPbasketball", 
                                "HickoryHigh", "SethPartnow", "SherwoodStrauss", "teamziller", "Pflanns", "treykerby", "LeighEllis", "TasMelas",
                                "jeskeets", "talkhoops", "MagicJohnson", "Barryathree", "blakegriffin32", "NickSwagyPYoung", "Dloading", "shaq",
                                "russellwestbrook", "dirknowitski", "mister6clutch", "bobbysacre", "Scalabrine", "RedMamba_15", "SeinfeldToday", "TurnerSportsEJ",
-                               "TheJetOnTNT"]
+                               "TheJetOnTNT", "RealStevenAdams", "arronafflalo", "colea45", "aldridge_12", "greenRAYn20", "aa000G9",
+                               "Adeto_kouboS", "agentzeroshow", "Gustavo_Ayon15", "ShaneBattier", "kobebryant", "mchalmers15", "boogiecousins",
+                               "StephenCurry30", "KDTrey5", "manuginobili", "Money23Green", "Jrue_Holiday11", "DwightHoward", 
+                               "Reggie_Jackson", "KingJames", "deandrejordan", "ZachLaVine", "kawhileonard", "Dame_Lillard", "tp9network",
+                               "ChandlerParsons", "AustinRivers25", "drose", "rickyrubio9", "KarlTowns", "KembaWalker", "DwyaneWade", 
+                               "youngwhiteside", "22wiggins", "IAmJustise", "MettaWorldPeace"]
                                
   HashTags = ["#America", "#4Life", "#politiciansofinstagram", "#keepit100", "#SwishinAndDishin", "#SurgingAndSplurging", "#LegislateFromTheBench",
                                 "#Freedom", "#Patriot", "#GunsAndButter", "#donateplz", "#corporationsunited", "#boardwalk", "#payup", "#SellinToChina",
