6d647527ba673164c1e29e566a9ad07944a4d984
palebeatz <timothyry@msn.com>
2016-08-25 21:10:27 -0600

    fixed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( welcome.css )
 Rails.application.config.assets.precompile += %w( left_border.png )
 Rails.application.config.assets.precompile += %w( right_border.png )
-Rails.application.config.assets.precompile += %w( border2.jpg )
+Rails.application.config.assets.precompile += %w( banner2.jpg )
